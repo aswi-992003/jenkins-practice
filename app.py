7894c821bad01223ca5b69c55ad33ebb1a72467f
Aswini <midhtech_02@Aswinis-Mac-mini.local>
2026-09-02 10:39:53 +0530

    Message to add.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-print("Hello from jenkins1")
-print("Hello from jenkins2")
-print("Hello from jenkins3")
+
+print("Hello from jenkins")
 print("webhook test from github")
+print("Good morning today is wednesday")
